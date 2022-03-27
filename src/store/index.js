ed89697206fe1cb451c39d3d470f3109c78cf212
moasko.dev <42101671+moasko@users.noreply.github.com>
2022-03-27 17:14:40 +0000

    not solved

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
-import {createStore} from 'redux'
+import {createStore,applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
 import rootReducer from './reducer'
 
-export const Store = createStore(rootReducer)
+export const Store = createStore(rootReducer,applyMiddleware(thunk))
